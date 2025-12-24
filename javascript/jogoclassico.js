8482390969b2d040c5c3f2ce5ee08f9fd93ecf89
Diogo dos Santos <kikumbanationsa@gmail.com>
2025-12-24 14:10:21 +0100

    Adicionei dimensão para outras telas...

--- javascript/jogoclassico.js
+++ javascript/jogoclassico.js
@@ -5,6 +5,14 @@ function mostra(){
 
     var itens = window.document.getElementById('itens')
 
-    itens.style.display = 'inline-block'
+    itens.style.display = 'inline-block' 
+
+    if (itens == 'inline-block'){
+
+        itens.style.display = 'none'
+    } else{
+
+        itens == 'inline-block'
+    }
 
 }
