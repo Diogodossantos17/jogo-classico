11a22e669e1976080f013f87e0ca1b744d0b61ec
Diogo dos Santos <kikumbanationsa@gmail.com>
2025-12-26 13:43:24 +0100

    Algumas alterações ainda no pendente..

--- javascript/jogoclassico.js
+++ javascript/jogoclassico.js
@@ -1,23 +1,9 @@
 
-
 function mostra(){
 
-    const itens = window.document.getElementById('itens') 
+var itens = window.document.getElementById('itens')
 
-   itens.style.display = 'inline-block'
+itens.style.display = 'inline-block'
 
-
-   if (itens == 'inline-block'){
-    
-    itens = 'none'
-
-   } else{
-
-    itens.style.display = 'inline-block'
-   }
-
-
-  
 }
-
 
